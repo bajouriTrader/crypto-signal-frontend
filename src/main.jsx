e7c9928dad66600d6b5d01fbd263f87add50f04f
bajouriTrader <bajouri@gmail.com>
2026-07-10 00:37:00 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPanel from './AdminPanel.jsx'
 import './index.css'
+
+const RootComponent = window.location.hash === '#admin' ? AdminPanel : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>,
 )
