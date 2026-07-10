4873ff7372f8f547165bf6cb76d1b987a9590291
bajouriTrader <bajouri@gmail.com>
2026-07-10 18:24:20 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AdminPanel from './AdminPanel.jsx'
+import Login from './Login.jsx'
+import { getToken } from './auth.js'
 import './index.css'
 
-const RootComponent = window.location.hash === '#admin' ? AdminPanel : App
+function Root() {
+  const [authed, setAuthed] = useState(!!getToken())
+
+  if (!authed) {
+    return <Login onSuccess={() => setAuthed(true)} />
+  }
+
+  const RootComponent = window.location.hash === '#admin' ? AdminPanel : App
+  return <RootComponent />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RootComponent />
+    <Root />
   </React.StrictMode>,
 )
